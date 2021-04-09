8f784eca69929e83fa490dc214ed6875c8acc6a4
alex625051 <32332025+alex625051@users.noreply.github.com>
2021-04-09 06:10:59 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import time
+import requests
+from flask import Flask, redirect, url_for, request
+# from selenium import webdriver
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.keys import Keys
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/post')
+# def hello_world():
+#     r=requests.get('https://www.enforta.ru/contacts/branches/cherepovec.html', verify=False)
+#     ht=r.content
+#     print(ht)
+#
+#     # close web browser
+#     r.close()
+#     return ht
+
+@app.route('/get',methods = ['POST', 'GET'])
+def return_html():
+    if request.method == 'POST':
+        arges = dict(request.form.deepcopy())
+
+        print (arges)
+        print(request.args)
+        if arges.get("proxyUrl"):
+            url= arges.get("proxyUrl")
+            arges.pop("proxyUrl")
+            r = requests.post(url=url, data=arges, verify=False)
+            ht = r.content
+            # close web browser
+            r.close()
+            return ht
+        else:
+            return "Неудачные параметры"
+    else:
+        arges = dict(request.args.deepcopy())
+        print(arges)
+        if arges.get("proxyUrl"):
+            url= arges.get("proxyUrl")
+            arges.pop("proxyUrl")
+            r = requests.get(url=url, params=arges, verify=False)
+            ht = r.content
+
+            # close web browser
+            r.close()
+            return ht
+        else:
+            return "Неудачные параметры"
+
+if __name__ == '__main__':
+    app.run()
